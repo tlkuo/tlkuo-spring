cccf239771697250699ad5c36ca72674974747f0
martin <tlkuo.frb@gmail.com>
2016-06-27 11:34:28 +0800

    Initial Spring

---
+++ src/main/java/tw/frb/TlkuoSpringApplication.java
@@ -0,0 +1,12 @@
+package tw.frb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlkuoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlkuoSpringApplication.class, args);
+	}
+}
