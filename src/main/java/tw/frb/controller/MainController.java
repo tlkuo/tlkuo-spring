f2252a1de99cdb7103afdef750686c81a1cb1161
martin <tlkuo.frb@gmail.com>
2016-06-27 15:33:51 +0800

    Use Thymeleaf java template engine

--- src/main/java/tw/frb/controller/MainController.java
+++ src/main/java/tw/frb/controller/MainController.java
@@ -2,14 +2,12 @@ package tw.frb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 	
 	@RequestMapping(value = "/")
-	@ResponseBody
 	public String index() {
-		return "Welcome to Index Page";
+		return "index";
 	}
 }
